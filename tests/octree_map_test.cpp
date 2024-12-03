4e8c0ee522d9a6b60f11ebbb5507d3a5c78f8c87
Daniel Duberg <danielduberg@gmail.com>
2024-12-03 23:18:59 +0100

    Added test to check that it is possible to change the value of what nearest iterator returns

--- tests/octree_map_test.cpp
+++ tests/octree_map_test.cpp
@@ -37,6 +37,15 @@ TEST_CASE("Octree Map")
 
 	std::cout << std::endl;
 
+	for (auto& [m, d] : map.nearest(Vec3f{0, 2.9, 0})) {
+		std::cout << m.first << ": " << m.second << " with distance: " << d << std::endl;
+		if (2.8 > d) {
+			m.second = 100;
+		} else {
+			m.second = 200;
+		}
+	}
+
 	for (auto [m, d] : map.nearest(Vec3f{0, 2.9, 0})) {
 		std::cout << m.first << ": " << m.second << " with distance: " << d << std::endl;
 	}
@@ -45,6 +54,10 @@ TEST_CASE("Octree Map")
 
 	for (auto [m, d] :
 	     map.queryNearest(pred::Intersects(BS3({0, 1, 1.8}, 0.25)), Vec3f{0, 2.9, 0})) {
+		std::cout << m.first << ": " << m.second << " with distance: " << d << std::endl;
+	}
+
+	for (auto [m, d] : map.nearest(Vec3f{0, 2.9, 0})) {
 		std::cout << m.first << ": " << m.second << " with distance: " << d << std::endl;
 	}
 
